e3d449a47a0f98fd606f80787c407242a72bbdda
HildaMel <melanderhilda@gmail.com>
2017-12-18 15:35:44 +0100

    Update TableColumns.java

--- Assignment2/src/reddit_db/TableColumns.java
+++ Assignment2/src/reddit_db/TableColumns.java
@@ -1,21 +1,14 @@
 package reddit_db;
 
 public class TableColumns {
-	
-		public String id;
-	    public String parent_id;
-	    public String link_id;
-	    public String name;
-	    public String author;
-	    public String body;
-	    public String subreddit_id;
-	    public String subreddit;
-	    public int score;
-	    public int created_utc;
-
-	    @Override
-	    public String toString() {
-	        return "id: " + id + " - body: " + body;
-	}
-	
+    public String id;
+    public String parent_id;
+    public String link_id;
+    public String name;
+    public String author;
+    public String body;
+    public String subreddit_id;
+    public String subreddit;
+    public int score;
+    public int created_utc;	
 }
