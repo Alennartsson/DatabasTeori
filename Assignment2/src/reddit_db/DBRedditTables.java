a0ed5fa6cc050d3db096ce84a1c3b972c3a4244b
HildaMel <melanderhilda@gmail.com>
2017-12-25 21:34:00 +0100

    Update DBRedditTables.java

--- Assignment2/src/reddit_db/DBRedditTables.java
+++ Assignment2/src/reddit_db/DBRedditTables.java
@@ -13,14 +13,13 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DBRedditTables {
-	private String USER_TABLE = "USER";
-	private String REDDIT_TABLE = "REDDIT";
-	private String COMMENT_TABLE = "COMMENTS";
 	private Connection con;
 	private Statement st;
 	private String query;
-	private PreparedStatement pSt;
-	   
+	private long startTime;
+	private long time;
+	private ObjectMapper map = new ObjectMapper();
+
 	public DBRedditTables(Connection con) {
 		this.con = con;
 	}
@@ -28,101 +27,92 @@ public class DBRedditTables {
 	public void createTables() {
 	try {
 		st = con.createStatement();
-	            query = "CREATE TABLE IF NOT EXISTS " + USER_TABLE +
-	                    " (author varchar(20) PRIMARY KEY)";
-
+	            query = "CREATE TABLE IF NOT EXISTS " + "USER" +
+	                    " (author varchar(20) PRIMARY KEY)";        
 	            st.executeUpdate(query);
-	            st.close();
 
-	            st = con.createStatement();
-	            query = "CREATE TABLE IF NOT EXISTS " + REDDIT_TABLE +
-	                    " (subreddit_id varchar(10) PRIMARY KEY AUTO_INCREMENT," +
-	                    "subreddit varchar(10) NOT NULL UNIQUE)";
+	            query = "CREATE TABLE IF NOT EXISTS " + "REDDIT" +
+	                    " (subreddit_id varchar(15) PRIMARY KEY," +
+	                    "subreddit varchar(15) NOT NULL UNIQUE)";
 	            st.executeUpdate(query);
-	            st.close();
 	            
-	            st = con.createStatement();
-	            query = "CREATE TABLE IF NOT EXISTS " + COMMENT_TABLE +
-	                    " (id varchar(10) PRIMARY KEY AUTO_INCREMENT," +
+	            query = "CREATE TABLE IF NOT EXISTS " + "COMMENTS" +
+	                    " (id varchar(10) PRIMARY KEY ," +
 	                    "parent_id varchar(10) NOT NULL ," +
-	                    "link_id varchar(255) NOT NULL , " +
-	                    "name varchar(255) NOT NULL ," +
+	                    "link_id varchar(20) NOT NULL , " +
+	                    "name varchar(20) NOT NULL ," +
 	                    "author varchar(20) NOT NULL ," +
 	                    "body varchar(255) NOT NULL ," +
-	                    "subreddit_id varchar(10) NOT NULL ," +
-	                    "subreddit varchar(10) NOT NULL ,"+
-	                    "score INT(10) NOT NULL ," +
+	                    "subreddit_id varchar(15) NOT NULL ," +
+	                    "subreddit varchar(15) NOT NULL ,"+
+	                    "score INT(100) NOT NULL ," +
 	                    "create_utc INT(10) NOT NULL , " +
-	                    "FOREIGN KEY (author) REFERENCES " + USER_TABLE + "(author) ON DELETE CASCADE," +
-	                    "FOREIGN KEY (subreddit_id) REFERENCES " + REDDIT_TABLE + "(subreddit_id) ON DELETE CASCADE," +
-	                    "FOREIGN KEY (subreddit) REFERENCES " + REDDIT_TABLE + "(subreddit) ON DELETE CASCADE)";
+	                    "FOREIGN KEY (author) REFERENCES " + "USER" + "(author) ON DELETE CASCADE," +
+	                    "FOREIGN KEY (subreddit_id) REFERENCES " + "REDDIT" + "(subreddit_id) ON DELETE CASCADE," +
+	                    "FOREIGN KEY (subreddit) REFERENCES " + "REDDIT" + "(subreddit) ON DELETE CASCADE)";
 	            st.executeUpdate(query);
 	            st.close();
 
 	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        }
-	    }
-
+	    } 
 
 	    public void importData(File file) {
-	        Long startime = System.nanoTime();
-
-	        ObjectMapper mapper = new ObjectMapper();
-	        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+	    	System.out.println("Importing data...");
+	        startTime = System.nanoTime();
+	        map.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 	        try {
-	            con.setAutoCommit(false);
-
-	            PreparedStatement userStatement = con.prepareStatement
-	                    ("INSERT OR IGNORE INTO " + USER_TABLE + " VALUES(?)");
-	            PreparedStatement commentStatement = con.prepareStatement
-	                    ("INSERT OR IGNORE INTO " + COMMENT_TABLE + " VALUES(?,?,?,?,?,?,?,?,?,?)");
-	            PreparedStatement subredditStatement = con.prepareStatement
-	                    ("INSERT OR IGNORE INTO" + REDDIT_TABLE + " VALUES(?,?)");
-
+	            PreparedStatement userSt = con.prepareStatement
+	                    ("INSERT IGNORE INTO USER VALUES(?)");
+	            PreparedStatement commentSt = con.prepareStatement
+	                    ("INSERT IGNORE INTO COMMENTS VALUES(?,?,?,?,?,?,?,?,?,?)");
+	            PreparedStatement subredditSt = con.prepareStatement
+	                    ("INSERT IGNORE INTO REDDIT VALUES(?,?)");
 
 	            int lCount = 0;
 	            BufferedReader br= new BufferedReader(new FileReader(file));
 	            String line = br.readLine();
 	            while (line != null) {
-	                TableColumns obj = mapper.readValue(line, TableColumns.class);
-	                userStatement.setString(1, obj.author);
+	                TableColumns obj = map.readValue(line, TableColumns.class);
+	                userSt.setString(1, obj.author);
+	                
+	                subredditSt.setString(1, obj.subreddit_id);
+	                subredditSt.setString(2, obj.subreddit);
 
-	                commentStatement.setString(1, obj.id);
-	                commentStatement.setString(2, obj.parent_id);
-	                commentStatement.setString(3, obj.link_id);
-	                commentStatement.setString(4, obj.name);
-	                commentStatement.setString(5, obj.author);
-	                commentStatement.setString(6, obj.body);
-	                commentStatement.setString(7, obj.subreddit_id);
-	                commentStatement.setString(8, obj.subreddit);
-	                commentStatement.setInt(9, obj.score);
-	                commentStatement.setInt(10, obj.created_utc);
+	                commentSt.setString(1, obj.id);
+	                commentSt.setString(2, obj.parent_id);
+	                commentSt.setString(3, obj.link_id);
+	                commentSt.setString(4, obj.name);
+	                commentSt.setString(5, obj.author);
+	                commentSt.setString(6, obj.body);
+	                commentSt.setString(7, obj.subreddit_id);
+	                commentSt.setString(8, obj.subreddit);
+	                commentSt.setInt(9, obj.score);
+	                commentSt.setInt(10, obj.created_utc);
 
-	                subredditStatement.setString(1, obj.subreddit_id);
-	                subredditStatement.setString(2, obj.subreddit);
-					
-	                userStatement.executeUpdate();
-	                subredditStatement.executeUpdate();
-	                commentStatement.executeUpdate();
+	                userSt.executeUpdate();
+	                subredditSt.executeUpdate();
+	                commentSt.executeUpdate();
 	                lCount++;
 	                line = br.readLine();
 	            }
 
 	            con.commit();
 	            con.setAutoCommit(true);
-	            System.out.println(lCount + " items inserted, time: " + (System.nanoTime() - startime) / 1000000 + "ms");
+	            time = (System.nanoTime() - startTime) / 1000000000;
+	            System.out.println("Done " + lCount + " objects. Time: " + time);
 	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
-	}
-	   
-	public void clearTables() {
+	    }
+	    
+	public void dropTables() {
 		try {
 			st = con.createStatement();
-			query = "DROP TABLE IF EXISTS " + USER_TABLE + COMMENT_TABLE + REDDIT_TABLE ;
+			query = "DROP TABLE IF EXISTS USER, REDDIT, COMMENTS";
 			st.execute(query);
 		}catch(SQLException e) {
 			e.printStackTrace();
