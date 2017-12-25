a0b0916266b4084bb9c3f0c47f4c302c5ff3c30d
Zirhm <melanderhilda@gmail.com>
2017-12-25 21:57:55 +0100

    Update CarRental.java

--- Assignment3/assign3/CarRental.java
+++ Assignment3/assign3/CarRental.java
@@ -23,29 +23,7 @@ public class CarRental {
 	public void createTables() {
 		try {
 			st = con.createStatement();
-		            query = "CREATE TABLE IF NOT EXISTS " + "USER" +
-		                    " (author varchar(20) PRIMARY KEY)";        
-		            st.executeUpdate(query);
-
-		            query = "CREATE TABLE IF NOT EXISTS " + "REDDIT" +
-		                    " (subreddit_id varchar(15) PRIMARY KEY," +
-		                    "subreddit varchar(15) NOT NULL UNIQUE)";
-		            st.executeUpdate(query);
-		            
-		            query = "CREATE TABLE IF NOT EXISTS " + "COMMENTS" +
-		                    " (id varchar(10) PRIMARY KEY ," +
-		                    "parent_id varchar(10) NOT NULL ," +
-		                    "link_id varchar(20) NOT NULL , " +
-		                    "name varchar(20) NOT NULL ," +
-		                    "author varchar(20) NOT NULL ," +
-		                    "body varchar(255) NOT NULL ," +
-		                    "subreddit_id varchar(15) NOT NULL ," +
-		                    "subreddit varchar(15) NOT NULL ,"+
-		                    "score INT(100) NOT NULL ," +
-		                    "create_utc INT(10) NOT NULL , " +
-		                    "FOREIGN KEY (author) REFERENCES " + "USER" + "(author) ON DELETE CASCADE," +
-		                    "FOREIGN KEY (subreddit_id) REFERENCES " + "REDDIT" + "(subreddit_id) ON DELETE CASCADE," +
-		                    "FOREIGN KEY (subreddit) REFERENCES " + "REDDIT" + "(subreddit) ON DELETE CASCADE)";
+		            query =
 		            st.executeUpdate(query);
 		            st.close();
 
