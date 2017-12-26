2c9944d6451cc2dfc59d094ab612d3bf51cb8694
Zirhm <melanderhilda@gmail.com>
2017-12-26 18:33:08 +0100

    Update CarRental.java

--- Assignment3/assign3/CarRental.java
+++ Assignment3/assign3/CarRental.java
@@ -23,48 +23,58 @@ public class CarRental {
 	public void createTables() {
 		try {
 			st = con.createStatement();
+			
+			query = "CREATE TABLE IF NOT EXISTS CAR" +
+			    	 " (licence_plate varchar(10) PRIMARY KEY," +
+			         "price_car int(50) NOT NULL,"+
+			         "car_free varchar(10) NOT NULL,"+
+			         "model varchar(25) NOT NULL , "+
+			         "seats int(15) NOT NULL, "+
+			         "brand varchar(25) NOT NULL ,"+
+			         "rented varchar(10) NOT NULL,"+
+			         "production_year int(10) NOT NULL,"+
+			         "colour varchar(125) NOT NULL," +
+			         "mileage int(50) NOT NULL)";
+			         
 			query = "CREATE TABLE IF NOT EXISTS SERVICE" +
-	          		" (cars_service int(8) PRIMARY KEY," +
-					"last_service int(8) NOT NULL, "+
-					"problem varchar(40) NOT NULL)";    
+	          		" (service_stamp_number INT(10) PRIMARY KEY, "+
+					"licence_plate varchar(10) NOT NULL, " +
+	          		"car_service varchar(10) NOT NULL, " +
+					"last_service int(15) NOT NULL, "+
+					"problem varchar(100) NOT NULL)";    
 	        st.executeUpdate(query);
 	            	
 	        query = "CREATE TABLE IF NOT EXISTS PRICE" +
-		            " (price_car int(10) PRIMARY KEY, "+
-	            	"total_price int(100) NOT NULL," +
-	            	"car_breaks_price int(10) NOT NULL, " +
-	            	"mileage_price int(10) NOT NULL)";
+		            " (receipt_number INT(20) PRIMARY KEY, " +
+	        		"licence_plate VARCHAR(10) NOT NULL, " +
+		            "id_number_licence INT(25) NOT NULL, " +
+	        		"price_car int(25) NOT NULL, "+
+	            	"total_price int(50) NOT NULL," +
+	            	"car_breaks_price int(25) NOT NULL, " +
+	            	"mileage_price int(25) NOT NULL)";
+	            	//"FOREIGN KEY(licence_plate) REFERENCES " + "CAR" + " (licence_plate) ON DELETE CASCADE)";
 	        st.executeUpdate(query);
 	            
-			query = "CREATE TABLE IF NOT EXISTS CAR" +
-			    	 " (licence_plate varchar(15) PRIMARY KEY," +
-			         "cars_service int(8)," +
-			         "price_car int(10) NOT NULL,"+
-			         "cars_free varchar(5) NOT NULL,"+
-			         "model varchar(20) NOT NULL , "+
-			         "seats int(10) NOT NULL, "+
-			         "brand varchar(25) NOT NULL ,"+
-			         "rented varchar(5) NOT NULL,"+
-			         "production_year int(4) NOT NULL,"+
-			         "colour varchar(15) NOT NULL," +
-			         "mileage int(10) NOT NULL," +
-			         "FOREIGN KEY (price_car) REFERENCES " + "PRICE" + "(price_car) ON DELETE CASCADE, "+
-			         "FOREIGN KEY (cars_service) REFERENCES " + "SERVICE" + "(cars_service) ON DELETE CASCADE)";
+			         
+			         
+			         
+			         //"FOREIGN KEY (price_car) REFERENCES " + "PRICE" + "(price_car) ON DELETE CASCADE, "+
+			         //"FOREIGN KEY (cars_service) REFERENCES " + "SERVICE" + "(cars_service) ON DELETE CASCADE)";
 			st.executeUpdate(query);
 			            
+			/*
 		    query = "CREATE TABLE IF NOT EXISTS CUSTOMER" +
-		        	"(customer_name varchar(40) PRIMARY KEY ," +
+		        	"(id_number_licence varchar(25) PRIMARY KEY," +
+		        	"licence_plate varchar(10) NOT NULL ,"+
+		        	 "customer_name varchar(40) NOT NULL ," +
 		            "customer_personal_number int(10) NOT NULL ," +
-		            "card_number int(20) NOT NULL , " +
-		            "insurance varchar(20) NOT NULL ," +
-		            "id_number_licence varchar(20) NOT NULL ," +
-		            "rent_start_date varchar(255) NOT NULL ," +
-		            "rent_end_date varchar(15) NOT NULL ," +
-		            "licence_plate varchar(15) NOT NULL ,"+
-		            "total_price INT(100) NOT NULL," +
+		            "card_number int(50) NOT NULL , " +
+		            "insurance varchar(25) NOT NULL ," +
+		            "rent_start_date INT(10) NOT NULL ," +
+		            "rent_end_date INT(10) NOT NULL ," + 
 		            "FOREIGN KEY (licence_plate) REFERENCES " + "CAR" +  " (licence_plate) ON DELETE CASCADE)," +
 		           // "FOREIGN KEY (total_price) REFERENCES " + "PRICE" + " (total_price) ON DELETE CASCADE)";  
-		   st.executeUpdate(query);
+		   st.executeUpdate(query);*/
 		   st.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
